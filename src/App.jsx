e535caef68503cfc7ea8ac4399f0488c65a737a2
Facundo Montiel <montiel.facu@gmail.com>
2022-10-16 18:12:32 -0300

    Cree el componente Saludo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Saludo from "./Saludo"
+
 function App() {
 
   return (
-    <h1>
-      Hello Wolrd!
-    </h1>
+    <Saludo></Saludo>
   )
 }
 
---
+++ src/Saludo.jsx
@@ -0,0 +1,9 @@
+const Saludo = ()=>{
+    return(
+        <h1>
+            Hello Wolrd!
+        </h1>
+    );
+}
+
+export default Saludo;
